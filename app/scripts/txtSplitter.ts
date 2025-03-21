9fd7352611611c36cc6258063cb28abe4ee8af79
Dru-429 <ect.dhruv@gmail.com>
2025-03-21 10:40:56 +0530

    Added vector data into the Supabase

--- app/scripts/txtSplitter.ts
+++ app/scripts/txtSplitter.ts
@@ -3,17 +3,23 @@
 import data from "../assets/data";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { createClient } from '@supabase/supabase-js'
-
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase'
+import { OpenAIEmbeddings } from '@langchain/openai'
 
 const script = async () => {
 
     try {
         // const result = await fetch('./assets/data.txt');
         // const text = await result.text()
-        const text = data
-        const sbApiKey = process.env.SUPABASE_API_KEY
-        const sbUrl = process.env.SUPABASE_URL_ENDPOINT
-        const openAIApiKey = process.env.OPENAI_API_KEY
+        const text: string = data;
+        const sbApiKey: string | undefined = process.env.SUPABASE_API_KEY;
+        const sbUrl: string | undefined = process.env.SUPABASE_URL_ENDPOINT;
+        const openAIApiKey: string | undefined = process.env.OPENAI_API_KEY;
+        
+        if ( sbApiKey === undefined || sbUrl === undefined || openAIApiKey === undefined) {
+            throw new Error("env variables error ")
+            return
+        }
 
         const splitter = new RecursiveCharacterTextSplitter({
             chunkSize: 500,
@@ -22,8 +28,18 @@ const script = async () => {
         })
         const output = await splitter.createDocuments([text])
         
+        const client = createClient(sbUrl, sbApiKey)
 
-        console.log(output)
+        await SupabaseVectorStore.fromDocuments(
+            output,
+            new OpenAIEmbeddings({ openAIApiKey }),
+            {
+                client,
+                tableName: 'documents',
+            }
+        )
+        
+        // console.log(output)
     } catch (error) {
         console.log(error)
     }
